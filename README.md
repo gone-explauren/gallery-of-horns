71a700d53c242ee4719b42679732b601a67ac6c9
Laurel Perkins <lmperkins20@gmail.com>
2023-02-28 16:19:58 -0800

    features -02 and -03 complete

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # HornedBeasts
 
+* Vote for your favorite horned beast!
+
 ## Author: Laurel Perkins
 
 ## Feature Tasks
+
+### Lab-01
+
+* N/A
 
 ### Lab-02
 
@@ -13,7 +19,7 @@ Start time: 1:25PM
 
 Finish time: 1:47PM
 
-Actual time needed to complete: _____
+Actual time needed to complete: 22min
 
 * Number and name of feature: Allow Users to Favorite Individual Beasts
 
@@ -21,16 +27,29 @@ Estimate of time needed to complete: 1hr
 
 Start time: 1:48PM
 
-Finish time: _____
+Finish time: 2:33PM
 
-Actual time needed to complete: _____
+Actual time needed to complete: 45min
 
 * Number and name of feature: Bootstrap
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: 2hr
 
-Start time: _____
+Start time: 2:34PM
 
-Finish time: _____
+Finish time: 4:03PM
 
-Actual time needed to complete: _____
+Actual time needed to complete: 1hr, 29min
+
+## References
+
+### Lab-01
+
+* Thank you, TA Mike, for helping me understand React basics.
+
+### Lab-02
+
+* Thank you, Phil, for helping me implement Cards from Bootcamp.
+* Thank you, Kam, for helping me understand the CSS inspection tool in the browser.
+* Thank you, Instructor Sheyna, for helping me undestand how to stylize components imported from Bootstrap.
+* Thank you, Daniel, for helping me understand the difference between align-items and justify-conent in CSS using display: flex.
--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,18 +1,56 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 import './HornedBeasts.css';
 
+
+
 class HornedBeasts extends React.Component {
-	render () {
+	constructor(props) {
+
+		// super brings down all o the data from the previous component
+		super(props);
+
+		this.state = {
+			likes: 0,
+			liked: false
+		}
+	}
+
+
+	handleLikes = () => {
+		this.setState({
+			likes: this.state.likes + 1,
+			liked: true,
+		})
+	};
+
+
+	render() {
 		console.log(this.props);
 		return (
-			<article>
-			{/* "title" is passed down from parent component Main */}
-			<h2>{this.props.title}</h2>
-			<img src= {this.props.image_url} alt={this.props.title}></img>
-			<p>{this.props.description}</p>
-			</article>
+			<Card style={{ width: '18rem' }} className='p-1 h-100'>
+				<article className='hornedBeasts'>
+					<Card.Header>
+						<h2>{this.props.title}</h2>
+						{/* Ternary */}
+						<div>{this.state.liked ? '😍😍😍' : ''}</div>
+					</Card.Header>
+					<Card.Body>
+						<div className='hearts'>
+						{/* "title" is passed down from parent component Main */}
+						<p>{this.state.likes} &hearts;'s</p>
+						<p onClick={this.handleLikes}>💗</p>
+						</div>
+						<Card.Img
+							src={this.props.image_url}
+							alt={this.props.title}
+						/>
+						<p>{this.props.description}</p>
+					</Card.Body>
+				</article>
+			</Card >
 		);
-	}
-};
+	};
+}
 
 export default HornedBeasts;
