ac2c98cb5eeb306adf4af0580bfad3901e12795e
Laurel Perkins <lmperkins20@gmail.com>
2023-03-02 16:58:02 -0800

    styled form :)

--- src/components/BeastForm.js
+++ src/components/BeastForm.js
@@ -10,27 +10,34 @@ class BeastForm extends React.Component {
 	render() {
 
 		let selectedNumberHorns = this.props.filterData.map((beast, idx) => {
-		// console.log(beast);
-		<ListGroup.Item key={idx}>
-			{beast}
-		</ListGroup.Item>;
+			// console.log(beast);
+			<ListGroup.Item key={idx}>
+				{beast}
+			</ListGroup.Item>;
 		})
 
 		return (
 			<>
 				<Form onSubmit={this.props.handleSubmit}>
 
-					<Form.Label>
-						<Form.Control as="select" onChange={this.props.handleSelectionOnChange}>
-							<option value='all'>All</option>
-							<option value='one'>One</option>
-							<option value='two'>Two</option>
-							<option value='three'>Three</option>
-							<option value='more'>More</option>
-						</Form.Control>
-					</Form.Label>
+					<div className='form-style'>
+						<Form.Label>
+							<h3 className='form-header'>Filter Beasts by Number of Horns</h3>
+						</Form.Label>
 
-					<Button type="submit">Submit</Button>
+						<div className='select-and-submit'>
+							<Form.Control as="select" onChange={this.props.handleSelectionOnChange}>
+								<option value='all'>All</option>
+								<option value='one'>One</option>
+								<option value='two'>Two</option>
+								<option value='three'>Three</option>
+								<option value='more'>More</option>
+							</Form.Control>
+
+							<Button type="submit">Submit</Button>
+						</div>
+					</div>
+
 				</Form>
 
 				<ListGroup>
